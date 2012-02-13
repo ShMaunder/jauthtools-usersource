d2889a3d38446ded10253dc96b84018a26f6cb0f
Sam Moffatt <pasamio@gmail.com>
2012-02-13 00:30:18 -0800

    Updating sync plugin for 2.5

--- plugins/system/sync/sync.php
+++ plugins/system/sync/sync.php
@@ -1,20 +1,22 @@
 <?php
 /**
  * User Synchronization System
- * 
- * This syncs a user from an external source 
- * 
+ *
+ * This syncs a user from an external source
+ *
  * PHP4/5
- *  
+ *
  * Created on Apr 17, 2007
- * 
- * @package JAuthTools
- * @author Sam Moffatt <pasamio@gmail.com>
- * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
- * @copyright 2009 Sam Moffatt 
- * @version SVN: $Id:$
- * @see JoomlaCode Project: http://joomlacode.org/gf/project/jauthtools/
+ *
+ * @package     JAuthTools
+ * @subpackage  UserSource
+ * @author      Sam Moffatt <pasamio@gmail.com>
+ * @license     GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright   2012 Sam Moffatt
+ * @version     2.5.0
+ * @see         JoomlaCode Project: http://joomlacode.org/gf/project/jauthtools/
  */
+defined('_JEXEC') or die();
 
 jimport('joomla.plugin.plugin');
 jimport('jauthtools.usersource');
@@ -22,33 +24,34 @@ jimport('jauthtools.usersource');
 /**
  * User Synchronization
  * Start remote user synchronization
- * @package JAuthTools
- * @subpackage User-Source
+ *
+ * @package     JAuthTools
+ * @subpackage  UserSource
+ * @version     2.5.0
  */
-class plgSystemSync extends JPlugin {
-	/**
-	 * Constructor
-	 *
-	 * For php4 compatability we must not use the __constructor as a constructor for plugins
-	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-	 * This causes problems with cross-referencing necessary for the observer design pattern.
-	 *
-	 * @param object $subject The object to observe
-	 * @since 1.5
-	 */
-	function plgSystemSync(& $subject) {
-		parent :: __construct($subject);
-	}
+class plgSystemSync extends JPlugin
+{
 
-	function onAfterInitialise() {
+	/**
+	 * onAfterInitialise to trigger user sync for the curently logged in user.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.5
+	 */
+	function onAfterInitialise()
+	{
 		$plugin = & JPluginHelper :: getPlugin('system', 'sync');
 		$params = new JParameter($plugin->params);
-		$usersource = new JAuthUserSource(Array('demoteuser'=>$params->get('demoteuser',1)));
+		$usersource = new JAuthUserSource(Array('demoteuser' => $params->get('demoteuser', 1)));
 
-		if(($user = JFactory::getUser())) {
-			if($user->username) {
+		if (($user = JFactory::getUser()))
+		{
+			if ($user->username)
+			{
 				$usersource->doUserSynchronization($user->username);
 			}
 		}
 	}
+
 }
